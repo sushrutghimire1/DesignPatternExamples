2722ebad3e47652e2b2a7b762e358a6ff9bbe8f6
Sushrut Ghimire <sushrut.ghimire@clusus.com>
2021-04-18 19:25:41 +0530

    added template

---
+++ src/main/java/behavioural/template/Main.java
@@ -0,0 +1,11 @@
+package behavioural.template;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Cricket();
+        game.play();
+        System.out.println();
+        game = new Football();
+        game.play();
+    }
+}
